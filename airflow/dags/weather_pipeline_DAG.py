b71079e8dca258c01381748bdb72811317dbdecd
Attila <jn.attila@gmail.com>
2026-03-30 20:45:05 +0200

    Initial

--- airflow/dags/weather_pipeline_DAG.py
+++ airflow/dags/weather_pipeline_DAG.py
@@ -4,12 +4,21 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import sys
 
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.constants import ExecutionMode, TestBehavior
+from cosmos.profiles import DatabricksTokenProfileMapping
+
+
+
+
 sys.path.append('/opt/airflow/ingest')
 
 #from weather_fetcher import fetch_open_meteo_current
 #from storage_jsonl import append_jsonl, utc_now_iso
 #from config import ZUGLO_LAT, ZUGLO_LON, LOCATION_NAME
 from main import run
+
+
 
 
 def run_ingest():
@@ -36,14 +45,31 @@ with DAG(
         python_callable=run_ingest,
 
     )
+    profile_config = ProfileConfig(
+    profile_name="dbt_weather",
+    target_name="dev",
+    profiles_yml_filepath="/opt/airflow/dbt_weather/profiles.yml",
+)
 
-    dbt_run = BashOperator(
-        task_id="dbt_run",
-        bash_command="cd /opt/airflow/dbt/dbt_weather && dbt run --profiles-dir ."
-        
+    dbt_task_group = DbtTaskGroup(
+        group_id="dbt_weather_models",
+        project_config=ProjectConfig(
+            dbt_project_path="/opt/airflow/dbt_weather",
+        ),
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+            execution_mode=ExecutionMode.LOCAL,
+            dbt_executable_path="/home/airflow/.local/bin/dbt",
+        ),
+        operator_args={
+            "install_deps": True,
+        },
+        #test_behavior=TestBehavior.AFTER_ALL,
     )
 
-    ingest_task >> dbt_run
+    
+
+    ingest_task >> dbt_task_group
 
 
 
